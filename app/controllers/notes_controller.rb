7f2575bff280c398fae91e5d1e422df9792b0541
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-05-10 10:55:10 -0400

    set up notes MVC

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,9 @@
 class NotesController < ApplicationController
 
+    #before_filter :authenticate_student!
+
     def index 
+        #binding.pry
         @notes = Note.all 
     end
 
@@ -9,22 +12,41 @@ class NotesController < ApplicationController
     end
 
     def create 
-    end
-
-    def edit 
+        @note = Note.find_or_create_by(notes_params)
+        if @ride 
+            redirect_to note_path(@note)
+        else
+            render :new
+        end
     end
 
     def show
+        @note = Note.find(params[:id])
+        #binding.pry
     end
 
-    def destroy 
+    def edit
+        @note = Note.find_by(id: params[:id])
+    end
+
+    def update
+        @note = Note.find_by(id: params[:id])
+        @note.update(notes_params)
+        redirect_to note_path(@note)
     end
 
 
+    def destroy
+        @note = Note.find_by(params[:id])
+        @note.destroy
+        redirect_to notes_path                     
+    end
 
+    
     private 
 
     def notes_params
+        params.require(:note).permit(:title, :content)
     end
 
 end
