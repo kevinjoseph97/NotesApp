1f2befe971af984cf5ecf28fe25bea04f54676d1
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-05-09 20:03:42 -0400

    adds notes db

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,28 @@
+class NotesController < ApplicationController
+
+    def index 
+    end
+
+    def new 
+    end
+
+    def create 
+    end
+
+    def edit 
+    end
+
+    def show
+    end
+
+    def destroy 
+    end
+
+
+
+    private 
+
+    def notes_params
+    end
+    
+end
